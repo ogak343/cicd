5d38cf7c3e4c2049b827f4f5781f9ba9100d91fa
ogabek <khamdamov343@gmail.com>
2024-02-17 13:50:34 +0500

    master: fixing secrets reference

--- src/main/java/com/example/cicdpractice/CiCdPracticeApplication.java
+++ src/main/java/com/example/cicdpractice/CiCdPracticeApplication.java
@@ -2,12 +2,18 @@ package com.example.cicdpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CiCdPracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdPracticeApplication.class, args);
+    }
+
+    @GetMapping
+    public String cheer() {
+        return "Hello";
     }
 
 }
