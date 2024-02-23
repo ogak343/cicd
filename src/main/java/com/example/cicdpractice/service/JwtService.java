08db7e113cfe850c6a2386c84043bf89b3e75365
ogabek <khamdamov343@gmail.com>
2024-02-23 09:29:47 +0500

    Revert "master: fixing secrets reference"
    This reverts commit d98ee27714bb5640ef928b32712687eeaf7a7653.

--- src/main/java/com/example/cicdpractice/service/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.example.cicdpractice.service;
-
-import com.example.cicdpractice.entity.Account;
-
-public interface JwtService {
-    String generateToken(Account account);
-
-    void authorize(String string);
-}
--- src/main/java/com/example/cicdpractice/service/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.cicdpractice.service;
-
-import com.example.cicdpractice.entity.ConfirmationCode;
-
-public interface NotificationService {
-    void sendCode(ConfirmationCode code);
-}
