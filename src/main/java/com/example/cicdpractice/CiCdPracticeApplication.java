e457f2b40c07ffc76326c1519360d0782ca12931
khamd <khamdamov343@gmail.com>
2024-05-01 17:29:01 +0500

    dev: changes

--- src/main/java/com/example/cicdpractice/CiCdPracticeApplication.java
+++ src/main/java/com/example/cicdpractice/CiCdPracticeApplication.java
@@ -2,8 +2,9 @@ package com.example.cicdpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
 public class CiCdPracticeApplication {
 
     public static void main(String[] args) {
